6d2b19ba41845ad0f48f29e8c90a41663679601b
MichaelEvangelosD <13-13246@saeinstitute.edu>
2022-04-02 00:19:35 +0300

    Major code refactoring and work separation, see changes

---
+++ NodeGraphLibrary/Source_Code/GraphLibrary.cs
@@ -0,0 +1,229 @@
+﻿namespace NodeGraphLibrary
+{
+    /// <summary>
+    /// A class library containing all the necessary methods to manage an undirected graph - uses arrays.
+    /// </summary>
+    public static class GraphLibrary
+    {
+        static string[] vertexes = new string[5];
+        static string[,] vertexLinks = new string[6, 2];
+
+        /// <returns>The vertexes array</returns>
+        public static string[] GetVertexArray()
+        {
+            return vertexes;
+        }
+
+        /// <returns>The vertexes array</returns>
+        public static string[,] GetEdgesArray()
+        {
+            return vertexLinks;
+        }
+
+        //COMPLETED
+        #region NODE_CREATION
+        /// <summary>
+        /// Call to create a new vertex with the given name to the next empty array position
+        /// </summary>
+        /// <param name="vertexName">The name of the vertex</param>
+        /// <returns>True if the vertex was succesfully created.
+        /// <para>False is there was no space inside the array.</para></returns>
+        public static bool AddVertexToArray(string vertexName)
+        {
+            string simplifiedName = vertexName.Replace(" ", "").ToLower();
+
+            int index = GetEmptyArrayIndex(vertexes);
+
+            if (index != -1 && index >= 0)
+            {
+                vertexes[index] = simplifiedName;
+
+                //If we successfully created the vertex
+                return true;
+            }
+
+            //If the vertex list is full...
+            return false;
+        }
+        #endregion
+
+        //WIP
+        #region NODE_CONNECTING
+        public static void TryCreateVertexEdge(string vertexName, string edgeName)
+        {
+            //Validate that both names are present in the vertex array
+            if (SearchVertexNameExistance(vertexName) && SearchVertexNameExistance(edgeName))
+            {
+                //Check if the connection already exists
+                for (int i = 0; i < vertexLinks.GetLength(0); i++)
+                {
+                    if ((vertexLinks[i, 0] == vertexName && vertexLinks[i, 1] == edgeName)
+                        || (vertexLinks[i, 0] == edgeName && vertexLinks[i, 1] == vertexName))
+                    {
+                        //PrintColoredMessage(ConsoleColor.Red, "Connection already exists!");
+                    }
+                }
+
+                /*//Search for an empty space in the vertexes array
+                for (int i = 0; i < vertexLinks.GetLength(0); i++)
+                {
+                    //If the i position is empty - then both positions are empty...
+                    if (vertexLinks[i, 0] == null)
+                    {
+                        //...set the new connection entry
+                        vertexLinks[i, 0] = vertexName;
+                        vertexLinks[i, 1] = edgeName;
+
+                        //PrintColoredMessage(ConsoleColor.Green, $"Connected {vertexName} with {edgeName}");
+                        returnSentence = $"Connected {vertexName} with {edgeName}";
+                        return returnSentence;
+                    }
+                    else
+                    {
+                        returnSentence = ""
+                    }
+                }*/
+            }
+        }
+        #endregion
+
+        #region UTILITIES
+        /// <summary>
+        /// Call to check if there are any null spaces in the vertexes array
+        /// </summary>
+        /// <returns>False if there is a null space
+        /// <para>True if not</para></returns>
+        public static bool IsVertexArrayFull()
+        {
+            foreach (string item in vertexes)
+            {
+                if (item == null) return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Call to check if there is a null space in the 1st dimension of the edges array.
+        /// </summary>
+        /// <returns>False if there is a null space
+        /// <para>True if not</para></returns>
+        public static bool IsEdgesArrayFull()
+        {
+            for (int i = 0; i < vertexLinks.GetLength(0); i++)
+            {
+                if (vertexLinks[i, 0] == null) return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Call to search the vertexes array for the given name
+        /// </summary>
+        /// <param name="name">Name to search for.</param>
+        /// <returns>True if name is found, false otherwise</returns>
+        public static bool SearchVertexNameExistance(string name)
+        {
+            string simplifiedName = name.Replace(" ", "").ToLower();
+
+            for (int i = 0; i < vertexes.Length; i++)
+            {
+                if (simplifiedName == vertexes[i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Call to find the first null space in the given array.
+        /// </summary>
+        /// <returns>The position of the empty space.
+        /// <para>-1 if no empty space was found.</para></returns>
+        static int GetEmptyArrayIndex(string[] arrayToCheck)
+        {
+            for (int i = 0; i < arrayToCheck.Length; i++)
+            {
+                if (arrayToCheck[i] == null)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Call to find the first null space in the 1st dimension of the given array.
+        /// </summary>
+        /// <returns>The position of the empty space
+        /// <para>-1 if no empty space was found</para></returns>
+        static int GetEmptyArrayIndex(string[,] arrayToCheck)
+        {
+            for (int i = 0; i < arrayToCheck.GetLength(0); i++)
+            {
+                if (arrayToCheck[i, 0] == null)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Call to cycle through the given array and set BOTH entries of the given name to null
+        /// </summary>
+        /// <param name="name">The name to delete.</param>
+        /// <param name="edgesArray">The array to search for the name in.</param>
+        /// <returns>A string containing the deleted connections.</returns>
+        public static string SearchEdgeNameAndDelete(string name, string[,] edgesArray)
+        {
+            string deletionSentence = "";
+
+            for (int i = 0; i < edgesArray.GetLength(0); i++)
+            {
+                //If the current index is null then continue to the next one
+                if (edgesArray[i, 0] == null) continue;
+
+                //If the i index matches the given name then delete both array entries
+                if (edgesArray[i, 0] == name || edgesArray[i, 1] == name)
+                {
+                    DeleteVertexFromArray(edgesArray, i);
+
+                    deletionSentence += $"Deleted connection {edgesArray[i, 0]}-{edgesArray[i, 1]} " +
+                        $"from the connection list. \n";
+                }
+            }
+
+            return deletionSentence;
+        }
+
+        /// <summary>
+        /// Call to set the given index inside the given array to NULL
+        /// </summary>
+        /// <param name="vertArray">The array to modify.</param>
+        /// <param name="index">The array index to set to null.</param>
+        public static void DeleteVertexFromArray(string[] vertArray, int index)
+        {
+            vertArray[index] = null;
+        }
+
+        /// <summary>
+        /// Call to set the [index,0] and [index,1] array positions to NULL.
+        /// </summary>
+        /// <param name="vertArray">The 2D array to modify</param>
+        /// <param name="index">The array index to set to null.</param>
+        public static void DeleteVertexFromArray(string[,] vertArray, int index)
+        {
+            for (int i = 0; i < vertArray.GetLength(1); i++)
+            {
+                vertArray[index, i] = null;
+            }
+        }
+        #endregion
+    }
+}
---
+++ NodeGraphUI/NodeCreatorUI.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using NodeGraphLibrary;
+
+namespace NodeGraphUI
+{
+    class NodeCreatorUI
+    {
+        static void Main(string[] args)
+        {
+            new NodeCreatorUI().Execute();
+        }
+
+        /// <summary>
+        /// Execute the program
+        /// </summary>
+        public void Execute()
+        {
+            string answer = "";
+            int result;
+
+            while (!answer.Equals("0"))
+            {
+                DisplayMenu();
+                PrintSeparatorLines();
+
+                Console.Write("Choice: ");
+                answer = Console.ReadLine();
+
+                //Try parsing the user choice into an int
+                if (Int32.TryParse(answer, out result))
+                {
+                    //...and check if it is a valid choice
+                    SwitchOnAnswer(result);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Display selection menu
+        /// </summary>
+        void DisplayMenu()
+        {
+            Console.WriteLine("Please select an option" +
+                "\n 1) Create station" +
+                "\n 2) Create station connection" +
+                "\n 3) Print all station connections" +
+                "\n 4) Delete a station" +
+                "\n 5) Delete a station connection" +
+                "\n 0) Terminate program");
+        }
+
+        /// <summary>
+        /// Call to switch on the user menu choice
+        /// </summary>
+        /// <param name="selection">The user-selected number</param>
+        void SwitchOnAnswer(int selection)
+        {
+            switch (selection)
+            {
+                case 1: //Create a new vertex
+                    CreateVertexSequence();
+
+                    PrintSeparatorLines();
+                    break;
+                case 2: //Create a new edge between vertexes
+                    AskVertexLinkNames();
+
+                    PrintSeparatorLines();
+                    break;
+                case 3: //Display the edges between vertexes
+                    PrintSeparatorLines();
+
+                    DisplayVertexLinking();
+
+                    PrintSeparatorLines();
+                    break;
+                case 4: //Prompt the user to delete a vertex
+                    DeleteVertex();
+
+                    PrintSeparatorLines();
+                    break;
+                case 5: //Prompt the user to delete a vertex edge
+                    DeleteVertexEdge();
+
+                    PrintSeparatorLines();
+                    break;
+                case 0: //Terminate program
+                    Environment.Exit(1);
+                    break;
+                default: //When none of the above is selected
+                    PrintColoredMessage(ConsoleColor.Red, "Please select a valid option.");
+                    break;
+            }
+        }
+
+        //COMPLETED
+        #region NODE_CREATION
+        /// <summary>
+        /// Call to ask the user to provide a new node name and check if it exists inside the nodes array.
+        /// If it doesn't exist then add it to the nodes array.
+        /// </summary>
+        void CreateVertexSequence()
+        {
+            //Early exit if the vertex list is already full
+            if (GraphLibrary.IsVertexArrayFull())
+            {
+                PrintColoredMessage(ConsoleColor.Red, "Station list is full");
+                return;
+            }
+
+            //Prompt for vertex name
+            string givenName;
+            bool namesIsValid = false;
+
+            do
+            {
+                Console.Write("Station name: ");
+                givenName = Console.ReadLine();
+
+                if (GraphLibrary.SearchVertexNameExistance(givenName))
+                {
+                    PrintColoredMessage(ConsoleColor.Cyan, $"Station name {givenName} already exists!");
+                }
+                else
+                {
+                    namesIsValid = true;
+                }
+
+            } while (!namesIsValid);
+
+            //Create and add node to array
+            if (GraphLibrary.AddVertexToArray(givenName))
+            {
+                PrintColoredMessage(ConsoleColor.Green, $"Station {givenName} created");
+            }
+        }
+        #endregion
+
+        //WIP
+        #region NODE_CONNECTING
+        void AskVertexLinkNames()
+        {
+            //Early exit if the edges array is already full
+            if (GraphLibrary.IsEdgesArrayFull())
+            {
+                PrintColoredMessage(ConsoleColor.Red, "The connections list is full!");
+                return;
+            }
+
+            string vertexName, edgeName;
+
+            Console.WriteLine("Connect which station ?");
+            vertexName = Console.ReadLine();
+
+            Console.WriteLine($"Connect {vertexName} to ...?");
+            edgeName = Console.ReadLine();
+
+            PrintColoredMessage(ConsoleColor.Red, "NODE_CONNECTING is WIP");
+            //GraphLibrary.TryCreateVertexEdge(vertexName, edgeName);
+        }
+        #endregion
+
+        //COMPLETED
+        #region NODE_PRINTING
+        /// <summary>
+        /// Call to write all the contents of the vertexes array into the console
+        /// </summary>
+        void DisplayVertexesArray()
+        {
+            for (int i = 0; i < GraphLibrary.GetVertexArray().Length; i++)
+            {
+                Console.WriteLine($"Station {i}: {GraphLibrary.GetVertexArray()[i]}");
+            }
+        }
+
+        /// <summary>
+        /// Call to display all node connections from the nodeConnections array.
+        /// </summary>
+        static void DisplayVertexLinking()
+        {
+            Console.WriteLine("|Stations|\t|Connections|");
+
+            for (int i = 0; i < GraphLibrary.GetEdgesArray().GetLength(0); i++)
+            {
+                for (int j = 0; j < GraphLibrary.GetEdgesArray().GetLength(1); j++)
+                {
+                    Console.Write($"{i} {GraphLibrary.GetEdgesArray()[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        #endregion
+
+        //COMPLETED
+        #region NODE_DELETION
+        /// <summary>
+        /// Call to display the nodes array and prompt the user which one to delete.
+        /// <para>Marks the given array index as null</para>
+        /// </summary>
+        void DeleteVertex()
+        {
+            //Helper variables
+            string userChoice, deletedEdgesInfo;
+            int parsedAnswer;
+
+            //Ask the user to delete a node at least once
+            do
+            {
+                PrintSeparatorLines();
+
+                Console.WriteLine("Which station to delete?");
+                DisplayVertexesArray(); //Display the vertex array
+
+                //Cache and parse the user input
+                userChoice = Console.ReadLine();
+
+            } while (!Int32.TryParse(userChoice, out parsedAnswer));
+
+            //Check if the given answer is inside the bounds of the array
+            if (parsedAnswer >= 0 && parsedAnswer <= GraphLibrary.GetVertexArray().Length)
+            {
+                //Cache the vertex name before it gets deleted so we can use it 
+                //to search the edges array for the connections
+                string cachedVertexName = GraphLibrary.GetVertexArray()[parsedAnswer];
+
+                //Nullify the given index position inside the vertexes array
+                GraphLibrary.DeleteVertexFromArray(GraphLibrary.GetVertexArray(), parsedAnswer);
+
+                deletedEdgesInfo = GraphLibrary.SearchEdgeNameAndDelete(cachedVertexName, GraphLibrary.GetEdgesArray());
+
+                PrintColoredMessage(ConsoleColor.Red, deletedEdgesInfo);
+
+                PrintColoredMessage(ConsoleColor.Yellow, $"Deleted station from entry position number : {userChoice}");
+            }
+            else
+            {
+                PrintColoredMessage(ConsoleColor.Red, "Given number is out of bounds.");
+            }
+        }
+
+        /// <summary>
+        /// Call to prompt the user to delete a node connection from inside the nodeConnections array.
+        /// <para>If it exists both array indexes get nullified.</para>
+        /// </summary>
+        void DeleteVertexEdge()
+        {
+            //Helper variables
+            string userChoice;
+            int parsedAnswer;
+
+            //Ask the user to delete a connection at least once
+            do
+            {
+                PrintSeparatorLines();
+
+                Console.WriteLine("Which connection to delete?");
+                DisplayVertexLinking(); //Display the vertex edges array
+
+                //Cache and parse the user input
+                userChoice = Console.ReadLine();
+            } while (!Int32.TryParse(userChoice, out parsedAnswer));
+
+            //Check if the given answer is inside the bounds of the array
+            if (parsedAnswer >= 0 && parsedAnswer <= GraphLibrary.GetEdgesArray().GetLength(0))
+            {
+                //Clear both array entries
+                GraphLibrary.DeleteVertexFromArray(GraphLibrary.GetEdgesArray(), parsedAnswer);
+
+                PrintColoredMessage(ConsoleColor.Yellow, $"Deleted connection from entry position number : {userChoice}");
+            }
+            else
+            {
+                PrintColoredMessage(ConsoleColor.Red, "Given number is out of bounds.");
+            }
+        }
+        #endregion
+
+        #region UTILITIES
+        /// <summary>
+        /// Call to WriteLine the message in the console with the given color,
+        /// <para>color is set back to white after the message gets printed.</para>
+        /// </summary>
+        /// <param name="cColor">Color of the given sentence</param>
+        /// <param name="message">The message to display</param>
+        void PrintColoredMessage(ConsoleColor cColor, string message)
+        {
+            Console.ForegroundColor = cColor;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// Call to write 50 '-' to the console for STYLE
+        /// </summary>
+        void PrintSeparatorLines()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write("-");
+            }
+
+            Console.WriteLine();
+        }
+        #endregion
+    }
+}
